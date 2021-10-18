5f8e4c56ea89af285b4de725a64860bb693a6ee2
giffarialfarizy <giffarialfarizy@gmail.com>
2021-10-18 10:19:42 +0700

    ✨ arrays and objects

---
+++ objects.js
@@ -0,0 +1,14 @@
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+};
+// ninja.age = 'hai';
+ninja.age = 40;
+// ninja.skills = ['fighting', 'sneaking'];
+ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age: 40
+};
+console.log(ninja);
---
+++ objects.ts
@@ -0,0 +1,18 @@
+let ninja = {
+  name: 'mario',
+  belt: 'black',
+  age: 30,
+};
+
+// ninja.age = 'hai';
+ninja.age = 40;
+
+// ninja.skills = ['fighting', 'sneaking'];
+ninja = {
+  name: 'yoshi',
+  belt: 'orange',
+  age: 40,
+  // skills: 'abc';
+};
+
+console.log(ninja);
