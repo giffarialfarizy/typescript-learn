7ad79a89c17f052738341ffae9b5efd327c2f50b
giffarialfarizy <giffarialfarizy@gmail.com>
2021-10-21 09:23:49 +0700

    ✅ modules

--- public/app.js
+++ public/app.js
@@ -1,24 +1,4 @@
-"use strict";
-// classes
-class Invoice {
-    // readonly, private, amount
-    //   client: string;
-    //   private details: string;
-    //   public amount: number;
-    //   constructor(c: string, d: string, a: number) {
-    //     this.client = c;
-    //     this.details = d;
-    //     this.amount = a;
-    //   }
-    constructor(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    format() {
-        return `${this.client} owes $${this.amount} for ${this.details}`;
-    }
-}
+import { Invoice } from './classes/Invoice.js';
 const invOne = new Invoice('mario', 'work on website', 250);
 const invTwo = new Invoice('luigi', 'work on hardware', 300);
 let invoices = [];
