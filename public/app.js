9287a4cb220a4321eea5b0defee270b0c71270d5
giffarialfarizy <giffarialfarizy@gmail.com>
2021-10-21 21:54:13 +0700

    ✨ tuples

--- public/app.js
+++ public/app.js
@@ -12,9 +12,11 @@ const ul = document.querySelector('ul');
 const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    let values;
+    values = [tofrom.value, details.value, amount.valueAsNumber];
     let doc;
     if (type.value === 'invoice') {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
@@ -22,23 +24,15 @@ form.addEventListener('submit', (e) => {
     list.render(doc, type.value, 'end');
     // console.log(doc);
 });
-// ENUMS
-var ResourceType;
-(function (ResourceType) {
-    ResourceType[ResourceType["BOOK"] = 0] = "BOOK";
-    ResourceType[ResourceType["AUTHOR"] = 1] = "AUTHOR";
-    ResourceType[ResourceType["FILM"] = 2] = "FILM";
-    ResourceType[ResourceType["DIRECTOR"] = 3] = "DIRECTOR";
-    ResourceType[ResourceType["PERSON"] = 4] = "PERSON";
-})(ResourceType || (ResourceType = {}));
-const docOne = {
-    uid: 1,
-    resourceType: ResourceType.BOOK,
-    data: { title: 'name of the wind' },
-};
-const docTwo = {
-    uid: 10,
-    resourceType: ResourceType.PERSON,
-    data: { name: 'yoshi' },
-};
-console.log(docOne, docTwo);
+// tuples
+let arr = ['ryu', 25, true];
+arr[0] = false;
+arr[1] = 'yoshi';
+arr = [30, false, 'luigi'];
+let tup = ['ryu', 25, true];
+// tup[0] = false;
+tup[0] = 'ken';
+tup[1] = 30;
+let student;
+// student = [2235, 'ken'];
+student = ['chun-li', 2234];
